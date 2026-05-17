1516828fa145a4ee857659cea49df36f167fd1be
BevMutanu <bev4366@gmail.com>
2026-05-17 18:46:45 +0300

    Commit all changes

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { describe, test, expect } from 'vitest';
+import { describe, test, expect, beforeEach } from 'vitest';
 import App from './App';
 import blog from './Data/CreateBlog';
 
